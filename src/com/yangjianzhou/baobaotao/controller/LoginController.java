fa185a98373749d6fb1897f3a3b5f1e69e213025
yangjianzhou <yangjianzhouctgu@126.com>
2015-05-31 22:00:48 +0800

    add request mapping  and post url

--- src/com/yangjianzhou/baobaotao/controller/LoginController.java
+++ src/com/yangjianzhou/baobaotao/controller/LoginController.java
@@ -5,21 +5,26 @@ import com.yangjianzhou.baobaotao.bean.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.yangjianzhou.baobaotao.service.UserService ;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
 	@Autowired
 	private UserService userService;
 
+	@RequestMapping(method = RequestMethod.GET)
 	public String loginPage() {
 		return "login";
 	}
 
+	@RequestMapping(value="/check", method = RequestMethod.POST)
 	public ModelAndView loginCheck(HttpServletRequest request, LoginFromBean loginCommand) {
 		boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName(), loginCommand.getPassword());
 		if (!isValidUser) {
